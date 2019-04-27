47daffd74df39ac2b6b9902854b417a3983d87cf
pone <pone0317@gmail.com>
2019-04-27 22:04:46 +0800

    又把 height 拔掉了，在 README 有寫原因。也修正了一些小地方

--- block/block.py
+++ block/block.py
@@ -10,25 +10,24 @@ def sha256(data):
 
 
 class Block_header():
-    def __init__(self, prev_block, target, nonce, height):
+    def __init__(self, prev_block, target, nonce):
         self.version = "00000001"
         self.prev_block = prev_block
-        self.merkle_root = 0000000000000000000000000000000000000000000000000000000000000000
+        self.merkle_root = "0000000000000000000000000000000000000000000000000000000000000000"
         self.target = target
         self.nonce = nonce
-        self.height = height
 
 
 class Block_hash():
     def __init__(self, version, prev_block, merkle_root, target, nonce):
         unhash = '{}{}{}{}{}'.format(
-            version, prev_block, str(merkle_root), str(target), str(nonce))
+            version, prev_block, merkle_root, str(target), str(nonce))
         self.hash = sha256(unhash)
 
 
 class Block():
-    def __init__(self, prev_block, target, nonce, height):
-        block_header = Block_header(prev_block, target, nonce, height)
+    def __init__(self, prev_block, target, nonce):
+        block_header = Block_header(prev_block, target, nonce)
         block_hash = Block_hash(block_header.version, block_header.prev_block,
                                 block_header.merkle_root, block_header.target, block_header.nonce)
         self.block_header = block_header
