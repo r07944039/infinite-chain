e69be238eac79929677bca4a850ad35202bbed0a
pone <pone0317@gmail.com>
2019-04-27 21:27:09 +0800

    新增 height 並且拔掉 function

--- block/block.py
+++ block/block.py
@@ -1,32 +1,35 @@
 #!/usr/bin/env python3
-# coding: -*- utf-8 -*-
 
 import hashlib
 
 
 def sha256(data):
     m = hashlib.sha256()
-    m.update(data)
+    m.update(data.encode('utf-8'))
     return m.hexdigest()
 
 
-class block_header():
-    def __init__(self, prev_block, target, nonce):
+class Block_header():
+    def __init__(self, prev_block, target, nonce, height):
         self.version = "00000001"
         self.prev_block = prev_block
         self.merkle_root = 0000000000000000000000000000000000000000000000000000000000000000
         self.target = target
         self.nonce = nonce
+        self.height = height
 
 
-class block_hash():
+class Block_hash():
     def __init__(self, version, prev_block, merkle_root, target, nonce):
-        unhash = "{}{}{}{}{}".format(
-            str(version), str(prevblock), str(merkle_root), str(target), str(nonce))
+        unhash = '{}{}{}{}{}'.format(
+            version, prev_block, str(merkle_root), str(target), str(nonce))
         self.hash = sha256(unhash)
 
 
-class block():
-    def __init__(self, block_header, block_hash):
+class Block():
+    def __init__(self, prev_block, target, nonce, height):
+        block_header = Block_header(prev_block, target, nonce, height)
+        block_hash = Block_hash(block_header.version, block_header.prev_block,
+                                block_header.merkle_root, block_header.target, block_header.nonce)
         self.block_header = block_header
         self.block_hash = block_hash
