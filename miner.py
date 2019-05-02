9822a20234bdea0200d3859f60c023fda693752f
jill070503 <b03611033@ntu.edu.tw>
2019-05-02 13:23:23 +0800

    modify mining

--- miner.py
+++ miner.py
@@ -3,11 +3,11 @@
 import hashlib
 import store
 import os
-import random
+from block.block import Block
 
 def sha256(data):
     m = hashlib.sha256()
-    m.update(data)
+    m.update(data.encode('utf-8'))
     return m.hexdigest()
 
 class Miner:
@@ -23,19 +23,19 @@ class Miner:
         block = store.node.chain[height]
         header = block.block_header
         pre_string = header.version + header.prev_block + header.merkle_root + header.target
-        nonce = hex(os.urandom(random.randint(0, 2**32)))[2:]
+        nonce = os.urandom(4).hex()
         mine = pre_string + nonce
-        while sha256(mine) > header.target:
-            nonce = hex(os.urandom(random.randint(0, 2**32)))[2:]
+        while int(sha256(mine),16) > int(header.target,16):
+            nonce = os.urandom(4).hex()
             mine = pre_string + nonce
         
         # Add block into your block chain
-        new_block = Block(block.block_hash, header.target, nonce)
-        _add_new_block(new_block)
+        new_block = Block(block.block_hash.hash, header.target, nonce)
+        self._add_new_block(new_block)
 
         # Boardcast new block to network
-        sendHeader(new_block.block_hash, new_block.block_header, height)
-        # print(nonce)
+        #self.sendHeader(new_block.block_hash, new_block.block_header, height)
+        print(nonce)
 
         return nonce
     
