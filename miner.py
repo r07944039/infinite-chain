52c869bab3280e6b533e6e8f13a6bf3f9dec1fdc
jill070503 <b03611033@ntu.edu.tw>
2019-05-06 00:33:19 +0800

    remove __add_new_block

--- miner.py
+++ miner.py
@@ -3,9 +3,10 @@
 import hashlib
 import store
 import os
+import json
 from block.block import Block
 from store import debug
-from api import sendHeader_send
+from api import sendHeader_send, _add_new_block
 
 def sha256(data):
     m = hashlib.sha256()
@@ -14,10 +15,6 @@ def sha256(data):
 
 class Miner:
     # def __init__(self):
-
-    def _add_new_block(self, block):
-        store.node.chain.append(block)
-        store.node.height += 1
         
     def mining(self):
         height = store.node.height
@@ -27,6 +24,7 @@ class Miner:
         nonce = os.urandom(4).hex()
         nonce_count = int(nonce,16)
         mine = pre_string + nonce
+
         while int(sha256(mine),16) > int(header.target,16):
             nonce_count += 1
             nonce = '{0:08x}'.format(nonce_count)
@@ -34,10 +32,9 @@ class Miner:
                 nonce_count = 0
                 nonce = nonce[1:]
             mine = pre_string + nonce
-        
         # Add block into your block chain
         new_block = Block(block.block_hash, header.target, nonce)
-        self._add_new_block(new_block)
+        _add_new_block(new_block)
         
         # Boardcast new block to network
         sendHeader_send(new_block.block_hash, new_block.block_header.header, height)
