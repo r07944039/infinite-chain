fbd6f37645e3b2a1547cc8595fcfa152b1c30f32
jill070503 <b03611033@ntu.edu.tw>
2019-05-31 16:46:17 +0800

    hash計算加上beneficiary，balance改傳dict，add_new_block移到sendblock後

--- miner.py
+++ miner.py
@@ -27,10 +27,10 @@ class Miner:
         # FIXME: 刪掉 merkle tree，不知道會不會有什麼影響
         # pre_string = header.version + block.block_hash + \
         #     "0000000000000000000000000000000000000000000000000000000000000000" + header.target
-        pre_string = header.version + block.block_hash + header.target
+        pre_string = header.version + block.block_hash + sha256("") + header.target
         nonce = os.urandom(4).hex()
         nonce_count = int(nonce, 16)
-        mine = pre_string + nonce
+        mine = pre_string + nonce + header.beneficiary
 
         while int(sha256(mine), 16) > int(header.target, 16):
             nonce_count += 1
@@ -38,7 +38,7 @@ class Miner:
             if len(nonce) > 8:
                 nonce_count = 0
                 nonce = nonce[1:]
-            mine = pre_string + nonce
+            mine = pre_string + nonce + header.beneficiary
 
         # TODO: 要在這邊 verify 挖出來的 block 中
         # transaction 是否跟最新的 transaction 一樣 (從 node 拿 trans 出來 verify)
@@ -48,8 +48,9 @@ class Miner:
         # Add block into your block chain
         # FIXME: 寫死 transactions & balance 亂寫
         # TODO: 把 balance 存進去
-        new_block = Block(block.block_hash, sha256(""), header.target, nonce, self.beneficiary, [], 0)
-        self.node.add_new_block(new_block, False)
+        balance = header.balance.copy()
+        new_block = Block(block.block_hash, sha256(""), header.target, nonce, self.beneficiary, [], balance)
+        # self.node.add_new_block(new_block, False)
 
         # Boardcast new block to network
         # sendHeader_send(new_block.block_hash,
@@ -65,12 +66,13 @@ class Miner:
             "target": header.target,
             "nonce": header.nonce,
             "transactions": header.transactions,
-            'block_height': height
+            'block_height': height + 1
         }
 
         # TODO: sendBlock 之後要等對方回傳 error = 0 
         # 才能 Add new block, 加玩新的 block 後才能繼續挖
         self.p2p.broadcast(self.p2p.apib.sendBlock, arg)
+        self.node.add_new_block(new_block, False)
 
         print(nonce)
         return nonce
